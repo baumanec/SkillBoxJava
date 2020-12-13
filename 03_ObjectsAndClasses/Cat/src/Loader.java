b407816e166278a49628210919ab64c8f95fe774
baumanec <ibaumanec@gmail.com>
2020-12-13 14:16:40 +0500

    Lesson 1 done

--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -3,8 +3,51 @@ public class Loader
 {
     public static void main(String[] args)
     {
-        Cat cat = new Cat();
+        Cat kesha = new Cat();
+        System.out.println("Kesha weigh: " + kesha.getWeight());
 
-        System.out.println(cat.getStatus());
+        kesha.feed(116.0);
+        System.out.println("Kesha weigh: " + kesha.getWeight());
+
+        System.out.println();
+
+        Cat ryzhik = new Cat();
+        System.out.println("Ryzhik weigh: " + ryzhik.getWeight());
+
+        ryzhik.feed(72.0);
+        System.out.println("Ryzhik weigh: " + ryzhik.getWeight());
+
+        System.out.println();
+
+        Cat kesya = new Cat();
+        System.out.println("Kesya weigh: " + kesya.getWeight());
+
+        while (kesya.getStatus() != "Dead")
+        {
+            kesya.meow();
+            System.out.println("Kesya weigh: " + kesya.getWeight());
+            System.out.println(kesya.getStatus());
+        }
+
+        System.out.println();
+
+        Cat marti = new Cat();
+        System.out.println("Marti weigh: " + marti.getWeight());
+
+        while (marti.getStatus() != "Exploded")
+        {
+            marti.feed(1.0);
+            System.out.println("Marti weigh: " + marti.getWeight());
+            System.out.println(marti.getStatus());
+        }
+
+        System.out.println();
+
+        Cat maksimilian = new Cat();
+        System.out.println("Maksimilian weigh: " + maksimilian.getWeight());
+
+        Cat business = new Cat();
+        System.out.println("Business weigh: " + business.getWeight());
+
     }
 }
