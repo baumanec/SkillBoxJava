0730e57c7ab78c6a9f81bb3abc59b2341081ca62
ibaumanec <ibaumanec@gmail.com>
2021-01-26 20:12:50 +0500

    homework_4.3* is done

--- 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.3/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -2,23 +2,57 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String boxes = scanner.nextLine();
+  public static final int MAX_CONTAINERS = 12;
+  public static final int MAX_BOXES = 27;
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String boxes = scanner.nextLine();
+    int truck = 0;
+    int container = 0;
+    if (Integer.parseInt(boxes) == 0) {
+      System.out.println("Необходимо:");
+      System.lineSeparator();
+      System.out.println("грузовиков - " + truck + " шт.");
+      System.lineSeparator();
+      System.out.println("контейнеров - " + container + " шт.");
+      System.lineSeparator();
+    } else {
+      ++truck;
+      ++container;
 
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+      for (int box = 1; box <= Integer.parseInt(boxes); box++) {
+        if (box == 1) {
+          System.out.println("Грузовик: " + truck);
+          System.lineSeparator();
+          System.out.println("\tКонтейнер: " + container);
+          System.lineSeparator();
+        }
+
+        while ((box % MAX_BOXES != 0) && (box <= Integer.parseInt(boxes))) {
+          System.out.println("\t\tЯщик: " + box);
+          box++;
+        }
+        if (box % MAX_BOXES == 0) {
+          System.out.println("\t\tЯщик: " + box);
+        }
+        if ((container % MAX_CONTAINERS == 0) | (container > truck * MAX_CONTAINERS)) {
+          truck++;
+          System.out.println("Грузовик: " + truck);
+          System.lineSeparator();
+        }
+        if ((box % MAX_BOXES == 0) && (box < Integer.parseInt(boxes))) {
+          container++;
+          System.out.println("\tКонтейнер: " + container);
+          System.lineSeparator();
+        }
+      }
+      System.out.println("Необходимо:");
+      System.lineSeparator();
+      System.out.println("грузовиков - " + truck + " шт.");
+      System.lineSeparator();
+      System.out.println("контейнеров - " + container + " шт.");
+      System.lineSeparator();
     }
-
+  }
 }
