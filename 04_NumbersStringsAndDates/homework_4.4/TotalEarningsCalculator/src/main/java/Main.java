81a33ec798798487ec3ca472d379898405856b3a
ibaumanec <ibaumanec@gmail.com>
2021-01-30 17:56:27 +0500

    Task 2 in homework_4.4 done and changed .gitignore

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,34 @@
 public class Main {
-
   public static void main(String[] args) {
+    int vasyaIncome = 0;
+    int petyaIncome = 0;
+    int mashaIncome = 0;
+    int total = 0;
+    int space = 0;
+    int indexOfSpace = 0;
+    int i = 0;
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+    while (i < text.length()) {
+      if (i == text.indexOf(' ')) {
+        space++;
+        indexOfSpace = i;
+        String income = text.substring(0, indexOfSpace);
+        text = text.substring(indexOfSpace + 1);
+        i = i - income.length();
+        if (space == 3) {
+          vasyaIncome = Integer.valueOf(income);
+        }
+        if (space == 7) {
+          petyaIncome = Integer.valueOf(income);
+        }
+        if (space == 12) {
+          mashaIncome = Integer.valueOf(income);
+        }
+      }
+      total = vasyaIncome + petyaIncome + mashaIncome;
+      i++;
+    }
+    System.out.println(total);
   }
-
 }
