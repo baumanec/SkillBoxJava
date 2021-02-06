a378484fc013d1755d9456bfd0999edcbc8ea406
ibaumanec <ibaumanec@gmail.com>
2021-02-06 20:08:39 +0500

    Task 1 in homework_4.5 is done

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,23 @@
 public class Main {
 
   public static void main(String[] args) {
-
+    String text = "Вася заработал";
+    System.out.println(calculateSalarySum(text));
   }
 
-  public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+  public static int calculateSalarySum(String text) {
+    int salary;
+    int totalSalary = 0;
+    String[] parts = text.split(",\\s+");
+    for (int i = 0; i < parts.length; i++) {
+      String word = parts[i].replaceAll("[^0-9]", "");
+      if (word.equals("")) {
+        return 0;
+      } else {
+        salary = Integer.parseInt(word);
+        totalSalary += salary;
+      }
+    }
+    return totalSalary;
   }
-
 }
