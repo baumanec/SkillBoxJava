eed0d68e3722e9f0824c0cb42cc8ecdf6bc20518
ibaumanec <ibaumanec@gmail.com>
2021-02-07 01:00:57 +0500

    Additional task in homework_4.5 is done

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,12 +1,21 @@
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    String safe = searchAndReplaceDiamonds("5484 4578 <4589> 5485 <5777> 5896", "***");
+    System.out.println(safe);
+  }
 
-    }
-
-    public static String searchAndReplaceDiamonds(String text, String placeholder) {
-        // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+  public static String searchAndReplaceDiamonds(String text, String placeholder) {
+    for (int i = 0; i < text.length(); i++) {
+      if (!(text.contains("<") && text.contains(">"))) {
         return text;
+      }
+      if (text.isEmpty()) {
+        return "";
+      }
+      text = text.replaceAll("<.+?>", placeholder);
     }
+    return text;
+  }
 
 }
