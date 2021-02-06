dae5056f971fa9864ff749c9d20aad925a967df3
ibaumanec <ibaumanec@gmail.com>
2021-02-06 23:56:42 +0500

    Task 3 in homework_4.5 is done

--- 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/FullNameFormatterRegExp/src/main/java/Main.java
@@ -5,13 +5,29 @@ public class Main {
   public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
+    outerLoop:
     while (true) {
       String input = scanner.nextLine();
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+      String[] words = input.split(" ");
+      for (int i = 0; i < words.length; i++){
+        if (Character.isDigit(words[i].charAt(i))){
+          System.out.println("Введенная строка не является ФИО");
+          break outerLoop;
+        }
+        if ((words.length > 3) | (words.length == 1)) {
+          System.out.println("Введенная строка не является ФИО");
+          break outerLoop;
+        }
+      }
+      String lastName = words[0];
+      String firstName = words[1];
+      String patronymic = words[2];
+      System.out.println(
+          "Фамилия: " + lastName.trim() + "\nИмя: " + firstName.trim() + "\nОтчество: "
+              + patronymic.trim());
     }
   }
 
