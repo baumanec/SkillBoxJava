2f8739f7a4008f09f44674805c1efa4dece4f6ad
ibaumanec <ibaumanec@gmail.com>
2021-02-20 18:07:19 +0500

    homework_5.2 is done

--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,85 @@
+import java.util.Scanner;
+
 public class Main {
+
     private static TodoList todoList = new TodoList();
+    static String command = "";
+    static String todo = "";
+    static int index = 0;
 
     public static void main(String[] args) {
-        // TODO: написать консольное приложение для работы со списком дел todoList
+
+        Scanner scanner = new Scanner(System.in);
+        outerLoop:
+        while (true) {
+            String text = scanner.nextLine();
+            String[] words = text.split("\\s", 2);
+            for (int i = 0; i < words.length; i++) {
+                if (words[i].equals("ADD") | words[i].equals("LIST") | words[i].equals("EDIT")
+                    | words[i].equals("DELETE")) {
+                    command = words[i];
+                }
+                if (!(command.equals("ADD") | command.equals("LIST") | command.equals("EDIT")
+                    | command.equals("DELETE"))) {
+                    System.out.println(
+                        "Введите команду ADD(добавление), EDIT(изменение) или DELETE(удаление)");
+                    break;
+                }
+                if (words.length < 2 && !command.equals("LIST")) {
+                    System.out.println("Неверный формат ввода");
+                    break outerLoop;
+                }
+                if (i == 1) {
+                    todo = words[i];
+                }
+            }
+
+            if (command.equals("ADD")) {
+                if (Character.isDigit(words[1].charAt(0))) {
+                    String[] modifiedTodo = words[1].split("\\s", 2);
+                    for (int j = 0; j < modifiedTodo.length; j++) {
+                        if (j == 0) {
+                            index = Integer.parseInt(modifiedTodo[j]);
+                        }
+                        if (j == 1) {
+                            todo = modifiedTodo[j];
+                        }
+                    }
+                    todoList.add(index, todo);
+                    if (index > todoList.getTodos().size() | index < 0) {
+                        continue;
+                    }
+                } else {
+                    todoList.add(todo);
+                }
+            }
+            if (command.equals("LIST")) {
+                for (int i = 0; i < todoList.getTodos().size(); i++) {
+                    System.out.println(
+                        i + " - " + todoList.getTodos().get(i));
+                }
+            }
+            if (command.equals("EDIT")) {
+                String[] modifiedTodo = words[1].split("\\s", 2);
+                for (int i = 0; i < modifiedTodo.length; i++) {
+                    if (i == 0) {
+                        index = Integer.parseInt(modifiedTodo[i]);
+                    }
+                    if (i == 1) {
+                        todo = modifiedTodo[i];
+                    }
+                }
+                todoList.edit(todo, index);
+                if (index > todoList.getTodos().size() | index < 0) {
+                    continue;
+                }
+            }
+            if (command.equals("DELETE")) {
+                todoList.delete(Integer.parseInt(words[1]));
+                if (Integer.parseInt(words[1]) > todoList.getTodos().size()
+                    | Integer.parseInt(words[1]) < 0) {
+                }
+            }
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
+++ 05_ArraysAndCollections/homework_5.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,57 @@ import java.util.ArrayList;
 
 public class TodoList {
 
-    public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
-    }
+  ArrayList<String> todoList = new ArrayList<>();
+  String buffer = "";
 
-    public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
-    }
+  public void add(String todo) {
+    todoList.add(todo);
+    System.out.println("Добавлено дело \"" + todo + "\"");
+  }
 
-    public void edit(String todo, int index) {
-        // TODO: заменить дело на index переданным todo индекс,
-        //  проверьте возможность изменения
+  public void add(int index, String todo) {
+    if (index < 0 | index > todoList.size()) {
+      System.out.println("Невозможно добавить дело на указанный номер");
+      if (!buffer.equals(todo)) {
+        buffer = todo;
+      }
+    } else {
+      todoList.add(index, todo);
+      System.out.println("Добавлено дело \"" + todo + "\"" + " под номером " + index);
     }
+  }
 
-    public void delete(int index) {
-        // TODO: удалить дело находящееся по переданному индексу,
-        //  проверьте возможность удаления дела
+  public void edit(String todo, int index) {
+    if (index < 0 | index > todoList.size()) {
+      System.out.println("Дело с таким номером не существует");
+    } else {
+      System.out.println(
+          "Дело \"" + todoList.get(index) + "\"" + " заменено на \"" + todo + "\"");
+      todoList.remove(index);
+      todoList.add(index, todo);
     }
+  }
 
-    public ArrayList<String> getTodos() {
-        // TODO: вернуть список дел
-        return new ArrayList<>();
+  public void delete(int index) {
+    if (index < 0 | index >= todoList.size()) {
+      System.out.println("Дело с таким номером не существует");
+    } else {
+      System.out.println("Дело \"" + todoList.get(index) + "\" " + "удалено");
+      todoList.remove(index);
     }
+  }
+
+  public ArrayList<String> getTodos() {
+    String todos;
+    ArrayList<String> list = new ArrayList<>();
+    for (int i = 0; i < todoList.size(); i++) {
+      todos = todoList.get(i);
+      list.add(todos);
+      if (!buffer.isEmpty() && i == todoList.size() - 1) {
+        list.add(buffer);
+      }
+    }
+    return list;
+  }
 
 }
