8d06beefced4c95c414feffade7cffbf61253ba3
ibaumanec <ibaumanec@gmail.com>
2021-03-15 17:47:27 +0300

    homework_7.1 modified

--- 07_AdvancedOOPFeatures/homework_7.1/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.1/src/main/java/Main.java
@@ -1,25 +1,20 @@
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
-    public static final String STAFF_TXT = "data/staff.txt";
+  public static final String STAFF_TXT = "data/staff.txt";
 
-    public static void main(String[] args) {
-        List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
-        System.out.println(staff);
-        sortBySalaryAndAlphabet(staff);
-        for (Employee employee : staff){
-            System.out.println(employee);
-        }
+  public static void main(String[] args) {
+    List<Employee> staff = Employee.loadStaffFromFile(STAFF_TXT);
+    System.out.println(staff);
+    sortBySalaryAndAlphabet(staff);
+    for (Employee employee : staff) {
+      System.out.println(employee);
     }
+  }
 
-    public static void sortBySalaryAndAlphabet(List<Employee> staff) {
-        staff.sort((o1, o2) -> {
-            int salarySorting = o1.getSalary().compareTo(o2.getSalary());
-            if (salarySorting != 0) {
-                return salarySorting;
-            }
-            return o1.getName().compareTo(o2.getName());
-        });
-    }
+  public static void sortBySalaryAndAlphabet(List<Employee> staff) {
+    staff.sort(Comparator.comparingInt(Employee::getSalary).thenComparing(Employee::getName));
+  }
 }
