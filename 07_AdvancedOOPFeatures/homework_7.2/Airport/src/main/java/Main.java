9b9fc1b7ca2b2126873a8b5c0b37e249aa996275
ibaumanec <ibaumanec@gmail.com>
2021-03-17 12:11:17 +0300

    Task 2 in homework_7.2 is done

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -1,18 +1,31 @@
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
-
-import java.util.Collections;
+import com.skillbox.airport.Flight.Type;
+import com.skillbox.airport.Terminal;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) {
 
+  public static void main(String[] args) {
 
+  }
+
+  public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
+    //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+    Date currentDay = new Date();
+    List<Terminal> terminals = new ArrayList<>(airport.getTerminals());
+    List<Flight> flights = new ArrayList<>();
+    for (Terminal terminal : terminals) {
+      flights.addAll(terminal.getFlights());
     }
 
-    public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
-    }
+    return flights.stream()
+        .filter(flight -> flight.getDate().getTime() - currentDay.getTime() <= 7200000)
+        .filter(flight -> flight.getType().equals(Type.DEPARTURE))
+        .collect(Collectors.toList());
+  }
 
 }
