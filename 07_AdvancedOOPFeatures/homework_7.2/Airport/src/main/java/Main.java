9a7382f455c768e27ea057410c34cc341bc929ac
ibaumanec <ibaumanec@gmail.com>
2021-03-19 11:55:02 +0300

    Task 2 in homework_7.2 modified

--- 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
+++ 07_AdvancedOOPFeatures/homework_7.2/Airport/src/main/java/Main.java
@@ -3,27 +3,27 @@ import com.skillbox.airport.Flight;
 import com.skillbox.airport.Flight.Type;
 import com.skillbox.airport.Terminal;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) {
-
   }
 
   public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-    //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-    Date currentDay = new Date();
     List<Terminal> terminals = new ArrayList<>(airport.getTerminals());
     List<Flight> flights = new ArrayList<>();
     for (Terminal terminal : terminals) {
       flights.addAll(terminal.getFlights());
     }
 
+    Calendar currentTime = new GregorianCalendar();
+    currentTime.add(Calendar.HOUR_OF_DAY, 2);
     return flights.stream()
-        .filter(flight -> flight.getDate().getTime() - currentDay.getTime() <= 7200000)
+        .filter(flight -> flight.getDate().before(currentTime.getTime()))
         .filter(flight -> flight.getType().equals(Type.DEPARTURE))
         .collect(Collectors.toList());
   }
