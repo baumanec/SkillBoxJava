cd5ab5122b8a08dff5ba7ec3d4a5088c47f634af
ibaumanec <ibaumanec@gmail.com>
2021-04-01 20:38:08 +0300

    homework_11.1 is done

--- 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/FileUtils.java
@@ -1,6 +1,29 @@
+import java.io.File;
+
 public class FileUtils {
 
     public static long calculateFolderSize(String path) {
-        return 0;
+        long size = 0L;
+        File folder = new File(path);
+        File[] files = folder.listFiles();
+        assert files != null;
+        for (File file : files) {
+            size += calculateFileSize(file);
+        }
+        return size;
+    }
+
+    private static long calculateFileSize(File file) {
+        long fileSize = 0L;
+        if (file.isDirectory()) {
+            File[] newDirectory = file.listFiles();
+            assert newDirectory != null;
+            for (File newFile : newDirectory) {
+                fileSize += calculateFileSize(newFile);
+            }
+        } else {
+            fileSize = file.length();
+        }
+        return fileSize;
     }
 }
--- 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.1/src/main/java/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+
+  private static final long sizeOfKB = 1024;
+  private static final long sizeOfMB = (long) Math.pow(sizeOfKB, 2);
+  private static final long sizeOfGB = (long) Math.pow(sizeOfKB, 3);
+
+  public static void main(String[] args) {
+    while (true) {
+      System.out.println("Введите путь до папки:");
+      Scanner scanner = new Scanner(System.in);
+      String path = scanner.nextLine();
+      long size = 0;
+      try {
+        size = FileUtils.calculateFolderSize(path);
+      } catch (Exception ex) {
+        ex.printStackTrace();
+      }
+      if (size >= sizeOfKB && size < sizeOfMB) {
+        System.out
+            .println("Размер папки " + path + " составляет " + size / sizeOfKB + " КБ");
+      }
+      if (size >= sizeOfMB && size < sizeOfGB) {
+        System.out
+            .println("Размер папки " + path + " составляет " + size / sizeOfMB + " МБ");
+      }
+      if (size >= sizeOfGB) {
+        System.out
+            .println("Размер папки " + path + " составляет " + size / sizeOfGB + " ГБ");
+      }
     }
+  }
 }
