0407c9f42b5961d959d1baf49c1eb34003dc0efc
ibaumanec <ibaumanec@gmail.com>
2021-04-04 23:37:04 +0300

    homework_11.2 is done

--- 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/FileUtils.java
@@ -1,5 +1,37 @@
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class FileUtils {
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+        try {
+            Path source = Paths.get(sourceDirectory);
+            Path destination = Paths.get(destinationDirectory);
+            Files.walk(source)
+                .forEach(s -> copy(s, destination.resolve(source.relativize(s))));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    static void copy(Path source, Path destination) {
+        try {
+            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void copyDirectory(String sourceDirectory, String destinationDirectory) {
+        try {
+            File source = new File(sourceDirectory);
+            File destination = new File(destinationDirectory);
+            org.apache.commons.io.FileUtils.copyDirectory(source, destination);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
+++ 11_FilesAndNetwork/homework_11.2/src/main/java/Main.java
@@ -1,4 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+    FileUtils.copyFolder("C:/Users/t-fre/Desktop/source", "C:/Users/t-fre/Desktop/destination");
+    FileUtils.copyDirectory("C:/Users/t-fre/Desktop/test", "C:/Users/t-fre/Desktop/destination");
     }
 }
