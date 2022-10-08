33d1b9d2f86b65d4a97fff3728f5d808f168b266
AlexArt <ibaumanec@gmail.com>
2022-10-09 00:29:51 +0300

    homework 2 in SQLAndHibernate is done

--- SQLAndHibernate/SQLAndHibernate/src/main/java/Main.java
+++ SQLAndHibernate/SQLAndHibernate/src/main/java/Main.java
@@ -1,40 +1,30 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox?useSSL=false&serverTimezone=Europe/Moscow";
-        String user = "root";
-        String pass = "i7mf6ux3";
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
-            Statement statement = connection.createStatement();
-            statement.execute("SELECT * "
-                                    + "FROM purchaselist pl \n"
-                                    + "WHERE YEAR(pl.subscription_date ) = 2018;");
-            ResultSet resultSet = statement.executeQuery("SELECT pl.course_name, "
-                                                                + "COUNT(*) / (max(month(pl.subscription_date)) - min(month(pl.subscription_date)) + 1) average\n"
-                                                                + " FROM PurchaseList pl \n"
-                                                                + " GROUP BY pl.course_name;");
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-            while (resultSet.next()) {
-                String courseName = resultSet.getString("course_name");
-                String averageBuyInMonth = resultSet.getString("average");
-                System.out.println(courseName + " - " + averageBuyInMonth);
-            }
+        Session session = sessionFactory.openSession();
+        Course course = session.get(Course.class, 3);
+        Student student = session.get(Student.class, 1);
 
-            resultSet.close();
-            statement.close();
-            connection.close();
+        System.out.println("Название курса: " + course.getName() + System.lineSeparator()
+                + "Количество студентов на курсе: " + course.getStudentsCount());
+        System.out.println("Имя студента: " + student.getName() + System.lineSeparator()
+                + "Возвраст студента: " + student.getAge() + System.lineSeparator()
+                + "Дата регистрации: " + student.getRegistrationDate());
 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
+        sessionFactory.close();
     }
 
 }
